1f97d0f99a6941fba31d494e0e87fa1ac9962ef7
Andrew Mann <drewmann47@yahoo.co.uk>
2016-02-17 21:26:22 +0000

    Further refined boids.py and planned next functions to be refactored

--- Boids/boids.py
+++ Boids/boids.py
@@ -14,8 +14,13 @@ TODO:
 		2. try_to_match_speed_with_nearby_boids
 		3. fly_away_from_nearby_boids
 		4. fly_towards_middle
-	
-NOTE_1: Adding intensity factors 
+
+		
+GOOD THINKS TO THINK ABOUT (SMELLS):
+	DRY CONCEPT - DONE
+	NAMING - DONE
+	SIMPLFY CODE
+	NUMBER OF COLUMNS IN A LINE
 '''
 
 class BoidsMethod(object):
@@ -29,46 +34,23 @@ class BoidsMethod(object):
 		self.interval = 50
 		self.xlim = (-500,1500)
 		self.ylim = (-500,1500)
-		self.formation_distance = 10000
-		self.middle_gravitational_pull = 0.9
 		
 		self.positions  = self.generate_boids_flock( np.array(self.position_bounds[0:2]),np.array(self.position_bounds[2:4]) )
 		self.velocities = self.generate_boids_flock( np.array(self.velocity_bounds[0:2]),np.array(self.velocity_bounds[2:4]) )
 		
 		#Encapsulate into a discionary for readability
 		self.update = {'X Position': self.positions[0], 'Y Position':self.positions[1], 'X Velocity': self.velocities[0],'Y Velocity':self.velocities[1]}
-
-	def delploy_simulation(self):
-		figure = plt.figure()
-		axes = plt.axes(xlim=self.xlim, ylim=self.ylim)
-		self.scatter = axes.scatter(self.positions[0],self.positions[1])
-		anim = animation.FuncAnimation(figure, self.animate, frames = self.frames, interval = self.interval)
-		plt.show()
-
-	def animate(self, frame):
-	   self.update_boids() #self.positions, self.velocities
-	   self.scatter.set_offsets(self.positions.transpose())
-	   
-	def generate_boids_flock(self, lower_limits, upper_limits):
-		width = abs(upper_limits - lower_limits)
-		return (lower_limits[:, np.newaxis] + np.random.rand(2, self.count) * width[:, np.newaxis])
-	   
-
-	def update_boids(self):	
-		self.fly_towards_middle() # Fly towards the middle
-		self.fly_away_from_nearby_boids() # Fly away from nearby boids
-		self.try_to_match_speed_with_nearby_boids() # Try to match speed with nearby boids
-		self.move_according_to_velocities() # Move according to velocities
-	
+		
+		
 	def move_according_to_velocities(self):
 		for i in range(len(self.update['X Position'])):
-			self.update['X Position'][i]=self.update['X Position'][i]+self.update['X Velocity'][i]
-			self.update['Y Position'][i]=self.update['Y Position'][i]+self.update['Y Velocity'][i]
+			self.update['X Position'][i] = self.update['X Position'][i]+self.update['X Velocity'][i]
+			self.update['Y Position'][i] = self.update['Y Position'][i]+self.update['Y Velocity'][i]
 	
 	def try_to_match_speed_with_nearby_boids(self):
 		for i in range(len(self.update['X Position'])):
 			for j in range(len(self.update['X Position'])):
-				if (self.update['X Position'][j]-self.update['X Position'][i])**2 + (self.update['Y Position'][j]-self.update['Y Position'][i])**2 < self.formation_distance:
+				if (self.update['X Position'][j]-self.update['X Position'][i])**2 + (self.update['Y Position'][j]-self.update['Y Position'][i])**2 < 10000:
 					self.update['X Velocity'][i]=self.update['X Velocity'][i]+(self.update['X Velocity'][j]-self.update['X Velocity'][i])*0.125/len(self.update['X Position'])
 					self.update['Y Velocity'][i]=self.update['Y Velocity'][i]+(self.update['Y Velocity'][j]-self.update['Y Velocity'][i])*0.125/len(self.update['X Position'])
 	
@@ -82,10 +64,37 @@ class BoidsMethod(object):
 	def fly_towards_middle(self):
 		for i in range(len(self.update['X Position'])):
 			for j in range(len(self.update['X Position'])):
-				self.update['X Velocity'][i]=self.update['X Velocity'][i]+(self.update['X Position'][j]-self.update['X Position'][i])*0.01/len(self.update['X Position']) * self.middle_gravitational_pull
+				self.update['X Velocity'][i] = self.update['X Velocity'][i]+(self.update['X Position'][j]-self.update['X Position'][i])*0.01/len(self.update['X Position'])
 		for i in range(len(self.update['X Position'])):
 			for j in range(len(self.update['X Position'])):
-				self.update['Y Velocity'][i]=self.update['Y Velocity'][i]+(self.update['Y Position'][j]-self.update['Y Position'][i])*0.01/len(self.update['X Position'])  * self.middle_gravitational_pull
+				self.update['Y Velocity'][i]=self.update['Y Velocity'][i]+(self.update['Y Position'][j]-self.update['Y Position'][i])*0.01/len(self.update['X Position'])
+	
+	
+	
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	''' THE BELOW FUNCTIONS ARE DONE '''
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+	def generate_boids_flock(self, lower_bound, upper_bound):
+		width = abs(lower_bound - upper_bound)
+		return (lower_bound[:, np.newaxis] + np.random.rand(2, self.count) * width[:, np.newaxis])
+	
+	def delploy_simulation(self):
+		figure = plt.figure()
+		axes = plt.axes(xlim=self.xlim, ylim=self.ylim)
+		self.scatter = axes.scatter(self.positions[0],self.positions[1])
+		anim = animation.FuncAnimation(figure, self.animate, frames = self.frames, interval = self.interval)
+		plt.show()
+
+	def animate(self, frame):
+	   self.update_boids() #self.positions, self.velocities
+	   self.scatter.set_offsets(self.positions.transpose())
+	
+	def update_boids(self):	
+		self.fly_towards_middle() # Fly towards the middle
+		self.fly_away_from_nearby_boids() # Fly away from nearby boids
+		self.try_to_match_speed_with_nearby_boids() # Try to match speed with nearby boids
+		self.move_according_to_velocities() # Move according to velocities
 
 if __name__ == "__main__":
 	# load factors from yaml file
